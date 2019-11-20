db82f036f0c6075eed4883fbf7f3d24bddb67164
jmm9683 <jakemorrissey@utexas.edu>
2019-11-19 20:10:26 -0600

    editing fields risk reduction

--- frontend_risk_reduction/input_controller.py
+++ frontend_risk_reduction/input_controller.py
@@ -14,3 +14,19 @@ bp = Blueprint('input', __name__)
 @bp.route('/edit/<id>/<status>', methods=['POST'])
 def editdata(id):
     return
+
+
+@bp.route('/edit/description/<id>', methods=['POST'])
+def editdata_description(id):
+    db = get_db()
+    newDescription = request.form['newDescription']
+    executeString = 'UPDATE components SET description=\'' + newDescription + '\' WHERE id =' + id
+    component_info = db.execute(
+        executeString
+    )
+    db.commit()
+    return "Succesfully Updated the Description for ID = " + id
+
+
+    # find component in database and return it in json format
+
