ae73d30cfb23a58a7573756f5bd4731db2a677c3
Wesley Klock <wesclock777@gmail.com>
2019-11-20 11:45:40 -0600

    integration risk reduction

---
+++ combined_risk_reduction/table/README.md
@@ -0,0 +1,14 @@
+# frontend-risk reduction
+
+Setting up app
+
+```bash
+set FLASK_APP=init.py
+python init.py
+```
+
+Initializing database
+
+```bash
+flask init-db
+```
--- frontend_risk_reduction/input_controller.py
+++ frontend_risk_reduction/input_controller.py
@@ -11,6 +11,51 @@ from flask import request
 
 bp = Blueprint('input', __name__)
 
-@bp.route('/edit/<id>/<status>', methods=['POST'])
+@bp.route('/edit/<id>', methods=['POST'])
 def editdata(id):
-    return
+    if 'data' in request and request.method=='POST':
+        try:
+            db = get_db()
+            values = list(request['data'].values())
+            component_info = db.execute(
+                ''' UPDATE components
+                    SET contexturl=?,
+                      etag=?,
+                      redfishid=?,
+                      name=?,
+                      oem=?,
+                      description=?,
+                      address=?,
+                      status=?,
+                      last_update=?
+                    WHERE id =?
+                ''',values
+            )
+
+            return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+        except:
+            return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
+
+@bp.route('/add', methods=['POST'])
+def adddata():
+    if 'data' in request and request.method=='POST':
+        try:
+            db = get_db()
+            values = list(request['data'].values())
+            component_info = db.execute(
+                ''' INSERT INTO components contexturl=?,
+                  etag,
+                  redfishid,
+                  name,
+                  oem,
+                  description,
+                  address,
+                  status,
+                  last_update)
+               VALUES (?,?,?,?,?,?,?,?)"
+                ''',values
+            )
+
+            return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+        except:
+            return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
